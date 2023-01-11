55d3cf304fc9e994c676cd580957b845e0e0e968
Austen Moraes <austenmoraes@gmail.com>
2023-01-11 18:51:48 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,76 @@
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+const allItens = document.querySelectorAll('.item')
+
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0
+
+// Ir para posição específica
+
+allItens.forEach(function(item,idx1) {
+  item.addEventListener('click', function() {
+    idx = idx1
+    changeImage()
+    resetInterval()
+  });
+});
+
+// Slider Automático
+
+let interval = setInterval(run, 3000)
+
+function run() {
+  idx++
+  changeImage()
+
+  allItens.forEach((item,idx1) => {
+    if(idx1 === idx) {
+      item.classList.add('active')
+    } else {
+      item.classList.remove('active')
+    }
+  })
+}
+
+function resetInterval() {
+  clearInterval(interval)
+  interval = setInterval(run, 2200)
+}
+
+function changeImage() {
+  if(idx > img.length-1){
+    idx = 0
+  } else if(idx<0){
+    idx = img.length-1
+  }
+
+allItens.forEach((item,idx1) => {
+  if(idx1 === idx) {
+    item.classList.add('active')
+    } else {
+    item.classList.remove('active')
+  }
+})
+  
+  imgs.style.transform = `translateX(${-idx * 870}px)`
+}
+
+rightBtn.addEventListener('click', ()=> {
+  idx++
+
+  changeImage()
+  resetInterval()
+})
+
+leftBtn.addEventListener('click', ()=> {
+  idx--
+
+  changeImage()
+  resetInterval()
+})
+
+
+
+
